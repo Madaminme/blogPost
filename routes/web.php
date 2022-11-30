7cda7181114f07eb59f9669e1a26200c386e767e
Milesjon <madaminme@gmail.com>
2022-11-30 15:21:41 +0500

    clockwork optimizition

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Models\Category;
 
 Route::get('/', function(){
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
